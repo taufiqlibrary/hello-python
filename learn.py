96ee53e484e93ce51af05afb13f1445d7827fc61
Taufiq Kurniawan <taufiq.library@gmail.com>
2021-08-05 22:37:38 +0700

    Create learn.py
    Learn Python

---
+++ learn.py
@@ -0,0 +1,18 @@
+# -*- coding : utf-8 -*-
+"""
+@author : taufiqlibrary
+"""
+
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+y1 = [1, 3, 5, 3, 1, 3, 5, 3, 1]
+y2 = [2, 4, 6, 4, 2, 4, 6, 4, 2]
+plt.plot(x, y1, label="line L")
+plt.plot(x, y2, label="line H")
+plt.plot()
+
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+plt.title("Line Graph Example")
+plt.legend()
+plt.show()
