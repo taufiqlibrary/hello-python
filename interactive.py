8c8935729ad9014c30999c5df2185f37f9585148
taufiqlibrary <taufiq.library@gmail.com>
2021-08-06 00:34:42 +0700

    Create interactive.py
    Add interactive

---
+++ interactive.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  5 23:17:52 2021
+
+@author: taufiqlibrary
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+ys = 200 + np.random.randn(100)
+x = [x for x in range(len(ys))]
+
+plt.plot(x, ys, '-')
+plt.fill_between(x, ys, 195, where=(ys > 195), facecolor='g', alpha=0.6)
+
+plt.title("Fills and Alpha Example")
+plt.show()
